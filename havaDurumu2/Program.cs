e116bc0df6596b151727f78c50eecc37bef36a86
Yakup Bingöl <20010310007@ogrenci.bartin.edu.tr>
2024-02-27 13:03:41 +0300

    asd

--- havaDurumu2/Program.cs
+++ havaDurumu2/Program.cs
@@ -17,19 +17,24 @@ namespace HavaDurumu
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                getWeatherState();
+                
+            }
+           
+        }
 
+        public static GetWeatherState  getWeatherState()
+        {
             string input;
             Console.WriteLine("Lütfen Hava Durumunu Görmek İstediginiz Sehri Girin: ");
             input = Console.ReadLine();
             string connection = $"https://api.openweathermap.org/data/2.5/weather?q={input}&mode=xml&lang=tr&units=metric&appid=8f54418aedd0985932a74e410a5d5de0";
             XDocument weather = XDocument.Load(connection);
             var temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
-            var weatherstate = weather.Descendants("weather").ElementAt(0).Attribute("value").Value;
-            Console.WriteLine(input + " için sicaklik: " + temp + "--------- Hava Durumu: " + weatherstate);
-        }
-
-        public static GetWeatherState  getWeatherState(string temp, string weatherState)
-        {
+            var weatherState = weather.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            Console.WriteLine(input + " için sicaklik: " + temp + "--------- Hava Durumu: " + weatherState);
 
 
             return new GetWeatherState
